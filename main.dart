c8bafc7394f08b8d6a59926466cc157eed502267
Bruno <104316988+BRRRRR000@users.noreply.github.com>
2023-02-07 23:45:09 -0300

    codigo aula 1

---
+++ main.dart
@@ -0,0 +1,65 @@
+void main() {
+  //tarefa numero 1
+
+  print('tarefa 1');
+  print('\n');
+
+  var pi = 3.14;
+  var raio = 8;
+  var area = pi * raio * raio;
+
+  print('area do circulo $raio igual a $area');
+
+  print('\n');
+
+  // tarefa numero 2
+
+  print('tarefa 2');
+  print('\n');
+  var chapa = 01279703;
+  var banco = 10;
+  var hora = 220;
+  var sal = banco * hora;
+
+  print('chapa do funcionario $chapa salario $sal');
+
+  print('\n');
+
+  //tarefa numero 3
+
+  print('tarefa 3');
+  print('\n');
+
+  var vendedor = "inacio";
+  var salario = 1200;
+  var vendas = 100000;
+  var comissao = 15000;
+
+  var rendaliq = salario + comissao;
+
+  print('inacio recebe no total $rendaliq');
+
+  print('\n');
+
+  //tarefa numero 4
+
+  print('tarefa 4');
+  print('\n');
+  var q = 50;
+  var u = -12;
+  var j = 100;
+
+  print('resultado: ${q + u + j}');
+
+  print('\n');
+
+  //tarefa numero 5
+
+  print('tarefa 5');
+
+  print('\n');
+
+  var kj = 12000 / 23000;
+  var porcentagem = 100;
+  print('resposta é ${kj * porcentagem}%');
+}
